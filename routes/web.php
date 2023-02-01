dca9038c20899c4177b0eb470d4d2c26f88b2b9a
Claudio996 <claudioantonio.retamal@gmail.com>
2023-02-01 14:15:44 -0300

    MIGRATION AND CONECTION WITH POSTGRES

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-}); 
+});  */
 
 Route::get('/test', function () {
     return view('tasks/index');
